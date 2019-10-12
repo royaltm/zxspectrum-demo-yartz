b372a7826ca5bc3a8106bf41c9b612041534c75d
Rafal Michalski <royaltm75@gmail.com>
2019-10-12 19:05:07 +0200

    WTFPL

--- gdc_builders.rb
+++ gdc_builders.rb
@@ -1,3 +1,7 @@
+# Copyright © 2019 r-type/GDC (Rafał Michalski) <royal@yeondir.com>
+# This work is free. You can redistribute it and/or modify it under the
+# terms of the Do What The Fuck You Want To Public License, Version 2,
+# as published by Sam Hocevar. See the COPYING file for more details.
 require 'z80'
 require_relative 'gdc_layout'
 require_relative 'gdc_patterns'
